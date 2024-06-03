7bd7fee90e7f2502c7426e969df3e54b98b24855
doupongzeng <huangzeng2016@126.com>
2024-06-03 14:00:47 +0800

    feat: tanjiro ascii art

--- lua/community.lua
+++ lua/community.lua
@@ -13,4 +13,5 @@ return {
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.cpp" },
   { import = "astrocommunity.motion.nvim-surround"},
+  { import = "astrocommunity.editing-support.rainbow-delimiters-nvim"},
 }
