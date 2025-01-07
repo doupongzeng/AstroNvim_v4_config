578deb7af44d43214041dd735f9065ee6fba1f40
doupongzeng <huangzeng2016@126.com>
2025-01-07 14:02:06 +0800

    feat: add copilot

--- lua/community.lua
+++ lua/community.lua
@@ -12,6 +12,8 @@ return {
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.cpp" },
-  { import = "astrocommunity.motion.nvim-surround"},
-  { import = "astrocommunity.editing-support.rainbow-delimiters-nvim"},
+  { import = "astrocommunity.motion.nvim-surround" },
+  { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
+  { import = "astrocommunity.completion.copilot-cmp" },
+  { import = "astrocommunity.editing-support.copilotchat-nvim" },
 }
